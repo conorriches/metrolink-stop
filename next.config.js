72e70cbdd99cb401f0c65f2f61caefe41ef1d114
Conor <905676+conorriches@users.noreply.github.com>
2024-11-20 20:55:23 +0000

    Update API path for CORS

--- next.config.js
+++ next.config.js
@@ -32,7 +32,7 @@ module.exports = withPWA(
     async headers() {
       return [
         {
-          source: "/api/:path*",
+          source: "/api/*",
           headers: [
             { key: "Access-Control-Allow-Credentials", value: "true" },
             { key: "Access-Control-Allow-Origin", value: "*" }, 
