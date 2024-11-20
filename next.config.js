e976a7a09c9d784ce481e9bd343ab0ad22091ca5
Conor <905676+conorriches@users.noreply.github.com>
2024-11-20 21:09:16 +0000

    move CORS headers to vercel file

--- next.config.js
+++ next.config.js
@@ -29,18 +29,6 @@ module.exports = withPWA(
       };
       return config;
     },
-    async headers() {
-      return [
-        {
-          source: "/api/:path*",
-          headers: [
-            { key: "Access-Control-Allow-Origin", value: "*" }, 
-            { key: "Access-Control-Allow-Methods", value: "GET" },
-            { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
-          ]
-        }
-      ]
-    },
     async redirects() {
       return [
         {
